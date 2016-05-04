9a8a89761c02a43eea6b8e5a7dcc035ac38bcd6d
ArjunTeh <arjun_teh@utexas.edu>
2016-05-03 19:16:46 -0500

    the test actually works now

--- test/test.js
+++ test/test.js
@@ -1,44 +1,46 @@
 var assert = require('chai').assert;
 var calendar = require('../modules/calendar.js');
+var db = require("../modules/db.js");
+
 require("datejs");
 
 describe('Calendar', function() {
     describe('#mutualFreeTimes()', function () {
 	it('should return 0 when no freetimes', function () {
 	    var busies = [ 
-		{ start : "2016-04-13T07:00:00Z", end : '2016-04-13T09:00:00Z' },
-		{ start : "2016-04-13T08:00:00Z", end : '2016-04-13T10:00:00Z' },
-	    ]
-	    var startTime = '2016-04-13T07:00:00Z';
-	    var endTime = '2016-04-13T11:00:00Z';
-
-	    var meh = Date.parse(startTime);
-	    console.log(meh.toString());
-
+		{ start : "2016-04-13T07:00:00", end : '2016-04-13T09:00:00' },
+		{ start : "2016-04-13T08:00:00", end : '2016-04-13T10:00:00' },
+	    ];
+	    var startTime = '2016-04-13T07:00:00';
+	    var endTime = '2016-04-13T11:00:00';
+	    
+	    // var meh = Date.parse(startTime);
+	    // console.log(meh.toISOString());
+	    // console.log( "calculating free times" );
 	    var free = calendar.mutualFreeTimes( busies, startTime, endTime );
-	    console.log(free);
+	    // console.log( "finished free times" );
+	    // console.log(free);
 	    assert.isAbove(free.length, 0, "free has something");
+	    assert.equal(free.length, 1);
 	});
     });
 });
 
 
 describe('Calendar', function() {
-    describe('#mutualFreeTimes()', function () {
-	it('should return 4 freetimes', function () {
-	    var busies = [ 
-		{ start : "2016-04-13T07:00:00Z", end : '2016-04-13T09:00:00Z' },
-		{ start : "2016-04-13T08:00:00Z", end : '2016-04-13T10:00:00Z' },
-	    ]
-	    var startTime = '2016-04-13T06:00:00Z';
-	    var endTime = '2016-04-13T11:00:00Z';
-
-	    var meh = Date.parse(startTime);
-	    console.log(meh.toString());
-
-	    var free = calendar.mutualFreeTimes( busies, startTime, endTime );
-	    console.log(free);
-	    assert.isAbove(free.length, 0, "free has something");
+    describe('#FreeTimeswithDB()', function () {
+	it('should return 0 when no freetimes', function () {
+	    db.connect();
+	    
+	    var free;
+	    db.getSession(0)
+		.then(function (data) {
+		    free = calendar.mutualFreeTimes( busies, startTime, endTime );
+		    assert.isAbove(free.length, 0, "free has something");
+		    assert.equal(free.length, 1);
+		    console.log(free);
+		});
 	});
     });
 });
+
